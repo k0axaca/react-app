0d61d64fbf1c4a29b15a391c80f7e934874ae3cb
Kristina <kkraft423@gmail.com>
2021-05-18 21:25:46 -0400

    src code

---
+++ src/Icon.js
@@ -0,0 +1,39 @@
+const Icon = () => {
+    return (
+      <footer>
+        <div className="myIcon">
+          <a
+            href="https://www.instagram.com/k0axaca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram icon" id="instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kristinakraft1234/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin-in icon" id="linkedin"></i>
+          </a>
+          <a
+            href="https://twitter.com/k0axaca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter icon" id="twitter"></i>
+          </a>
+          <a
+            href="https://github.com/k0axaca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github icon" id="github"></i>
+          </a>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Icon;
+  
